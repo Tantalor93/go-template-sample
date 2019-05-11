bfba84e197545743d5479e067562b8d7bd6e9bfe
Ondrej Benkovsky <ondrej.benkovsky@wandera.com>
2019-05-11 19:37:34 +0200

    add variable example

--- main.go
+++ main.go
@@ -7,19 +7,20 @@ import (
 )
 
 type Holder struct {
-	Name string
+	Name  string
 	Value int
 	Names []string
 }
 
-func main()  {
+func main() {
 	holder := Holder{"Petr", 1, []string{"alfa", "beta", "charlie"}}
 
 	templateWithCondition(holder)
 	fmt.Println()
 	templateWithRange(holder)
+	fmt.Println()
+	templateWithVariable(holder)
 }
-
 
 func templateWithRange(holder Holder) {
 	tmpl := template.New("benky template")
@@ -33,6 +34,15 @@ func templateWithRange(holder Holder) {
 func templateWithCondition(holder Holder) {
 	tmpl := template.New("benky template")
 	tmpl, error := tmpl.Parse("Hello '{{.Name}}' and {{if eq .Value 0 }} value is zero! {{else}} value is non zero! {{end}}")
+	if error != nil {
+		panic("Unable to parse template: " + error.Error())
+	}
+	tmpl.Execute(os.Stdout, holder)
+}
+
+func templateWithVariable(holder Holder) {
+	tmpl := template.New("benky template")
+	tmpl, error := tmpl.Parse("{{ with $variable := .Name }} {{$variable}} {{ end }}")
 	if error != nil {
 		panic("Unable to parse template: " + error.Error())
 	}
